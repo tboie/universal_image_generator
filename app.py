a36127d0402fb6d455e7f2e0e3dd2f09d84b7803
tboie <timboie@gmail.com>
2022-11-02 10:04:59 -0400

    json files

--- app.py
+++ app.py
@@ -9,24 +9,19 @@ import random
 import re
 
 # load json files
-f_plants = open('json/plants.json')
-plants = json.load(f_plants)
-plants = plants["plants"]
-
-f_landforms = open('json/landforms.json')
-landforms = json.load(f_landforms)
-
-f_minerals = open('json/minerals.json')
-minerals = json.load(f_minerals)
-
-f_rocks = open('json/rocks.json')
-rocks = json.load(f_rocks)
-
-f_sea_species = open('json/sea_species.json')
-sea_species = json.load(f_sea_species)
-
-f_animals = open('json/animals.json')
-animals = json.load(f_animals)
+l_plants = json.load(open('json/plants.json'))["plants"]
+l_landforms = json.load(open('json/landforms.json'))
+l_minerals = json.load(open('json/minerals.json'))
+l_rocks = json.load(open('json/rocks.json'))
+l_sea_species = json.load(open('json/sea_species.json'))
+l_animals = json.load(open('json/animals.json'))
+l_time = json.load(open('json/animals.json'))
+l_elements1 = json.load(open('json/elements1.json'))
+l_elements2 = json.load(open('json/elements2.json'))
+l_patterns = json.load(open('json/patterns.json'))
+l_animal_species = json.load(open('json/animal_species.json'))
+l_structure1 = json.load(open('json/buildings_community.json'))
+l_structure2 = json.load(open('json/buildings_single.json'))
 
 # load Flask library
 app = Flask(__name__)
@@ -46,51 +41,25 @@ def run_script():
     else:
         phrase = ", " + phrase + ", "
 
-    time = random.choice(["midnight", "midday", "noon", "morning", "afternoon", "evening",
-                          "night", "dawn", "dusk", "twilight", "sunrise", "sunup", "sunset", "daylight", "dark", "black", "bright"])
-
-    land = random.choice(landforms)
-
-    geo = random.choice(rocks)
-
-    element1 = random.choice(
-        ["water", "wood", "fire", "flame", "earth", "metal", "air", "wind", "glass", "laser", "plasma", "lava", "smoke", "oceanic", "underwater", "deepsea", "aquatic"])
-
-    element2 = random.choice(["lightning", "space", "astro", "cosmos", "ice", "snow", "frost",
-                             "gas", "fog", "mist", "clouds", "hail", "blizzard", "storm", "rain", "icicles", "holograph"])
-
-    mineral = random.choice(minerals)
-
-    pattern = random.choice(["symmetries", "trees", "spirals", "meanders",
-                            "waves", "foams", "tessellations", "cracks", "stripes"])
-
-    plant_species = random.choice(plants)["species"]
-
-    plant_name = random.choice(plants)["name"]
-
-    animal_species = random.choice(["mite", "tick", "arthopod", "crustacean", "milli", "centipede", "spider", "scorpion", "pseudoscorpion", "harvestmen", "arachnid", "insect",
-                                    "beetle", "butterfly", "bee", "ant", "grasshopper", "amphibian", "reptile", "frog", "toad", "salamander", "newt", "caecilian",
-                                    "turtle", "tortois", "lizard", "snake", "fish", "mollusk", "shell", "cephalopod", "mammal", "primate",
-                                    "bird", "parasite", "parasite host", "worm", "helminth", "plankton", "microorganism",
-                                    "protozoan", "nematoad", "roundworm"])
-
-    animal = random.choice(animals)
+    time = random.choice(l_time)
+    land = random.choice(l_landforms)
+    geo = random.choice(l_rocks)
+    element1 = random.choice(l_elements1)
+    element2 = random.choice(l_elements2)
+    mineral = random.choice(l_minerals)
+    pattern = random.choice(l_patterns)
+    plant_species = random.choice(l_plants)["species"]
+    plant_name = random.choice(l_plants)["name"]
+    animal_species = random.choice(l_animal_species)
+    animal = random.choice(l_animals)
+    structure1 = random.choice(l_structure1)
+    structure2 = random.choice(l_structure2)
 
     # 1 in 8 chance of sea species
     if random.randint(1, 8) == 8:
-        sea_species = random.choice(sea_species)
+        sea_species = random.choice(l_sea_species)
     else:
         sea_species = ""
-
-    structure1 = random.choice(["twin city", "megacity", "megalopolis", "smart city",
-                                "metro city", "garden city", "conurbation", "metropolis",
-                                "municipality", "cosmopolitan city", "city state", "intermediary city",
-                                "global city", "gateway city", "urban hub", "power city", "neighborhood", "town", "cityscape"])
-
-    structure2 = random.choice(["factory", "church", "hut", "religious building",
-                                "skyscraper", "palace", "pyramid", "mansion",
-                                "house", "religious building", "ritual building", "shrine", "tomb", "grave", "graveyard",
-                                "refuse pit", "cathedral"])
 
     key_desc = "epic ultimate ultra elite supreme unique technological mythological advanced futuristic archeological archetypal seasonal natural creatures living being musical architectural electrical monumental color wisdom inspiration realistic imagination engineering instruments symbolism sculpture raw genius artistic energy motion emotional wild mechanical intellect"
     key_desc = key_desc.split(" ")
